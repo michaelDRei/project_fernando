24a7d96b98685d1573603e6c9a5f63c61ac71a83
michael.alves2 <michael.alves@neomed.tech>
2022-09-07 14:29:21 -0300

    add images

--- js/alert.js
+++ js/alert.js
@@ -1,8 +1,6 @@
 function alert_image(img){
   Swal.fire({
-    title: img,
-    text: 'Modal with a custom image.',
-    imageUrl: `assets/ritmo.png`,
+    imageUrl: `assets/${img}.png`,
     imageWidth: 400,
     imageHeight: 504,
     imageAlt: 'Custom image',
